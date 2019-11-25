9d1f0a13195f0fb33e37c9e4f37158f251b9247a
minikin <djminikin@gmail.com>
2019-11-25 17:08:17 +0100

    Cleam up

--- memory_leak_flutter_project/lib/main.dart
+++ memory_leak_flutter_project/lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
-import 'dart:io';
 
 void main() => runApp(MyApp());
 
@@ -18,69 +16,71 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
   final String title;
+
+  MyHomePage({Key key, this.title}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final int countImages = 1000;
+  final int width = 4096;
+  final int height = 3072;
+  List<String> _imageUrlList = [];
 
-  final int countImages = 70;
-  final int width = 320;
-  final int height = 240;
-
-  bool isLocal = true;
-
-  List<Widget> loadNetworkImages() {
-    List<Widget> randomImages = List();
-
-    for(int i = 0; i < countImages; i++) {
-      randomImages.add(
-          Center(
-              child: Image.network("https://loremflickr.com/$width/$height?random=$i")
-          )
-      );
-    }
-
-    return randomImages;
-  }
-
-  List<Widget> loadLocalImages(List<FileSystemEntity> files) {
-    return files.map((entity) => Center(child: Image.asset(entity.path))).toList();
-  }
-
-  Future<List<FileSystemEntity>> getLocalPathsToFile() async {
-    final pathRootDirectory = await getApplicationDocumentsDirectory();
-    final pathFolderTestImages = "${pathRootDirectory.path}/${width}x${height}";
-
-    final directory = Directory(pathFolderTestImages);
-
-    if(directory == null) {
-      return [];
-    }
-
-    return directory.listSync();
+  @override
+  void initState() {
+    setState(() => _imageUrlList = _buildImageUrlList());
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-
-    final images = !isLocal ? loadNetworkImages() : [];
-
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: isLocal ? FutureBuilder<List<FileSystemEntity>>(
-        future: getLocalPathsToFile(),
-        builder: (context, snapshot) {
-          return snapshot.hasData ? ListView(children: loadLocalImages(snapshot.data)) : Center(child: CircularProgressIndicator());
-        },
-      ) : ListView(
-            children: images,
+      body: SafeArea(
+        child: Container(
+          margin: const EdgeInsets.all(8),
+          child: ListView.builder(
+              itemCount: _imageUrlList.length,
+              itemBuilder: (_, index) {
+                return PhotoItem(
+                  imageUrl: _imageUrlList[index],
+                );
+              }),
+        ),
+      ),
+    );
+  }
+
+  List<String> _buildImageUrlList() {
+    List<String> _imageUrlList = [];
+    for (var i = 0; i < countImages; i++) {
+      _imageUrlList.add('https://loremflickr.com/$width/$height?random=$i');
+    }
+    return _imageUrlList;
+  }
+}
+
+class PhotoItem extends StatelessWidget {
+  final String imageUrl;
+  const PhotoItem({
+    @required this.imageUrl,
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey[200],
+      margin: const EdgeInsets.all(4),
+      child: Image.network(
+        imageUrl,
+        fit: BoxFit.cover,
       ),
     );
   }
